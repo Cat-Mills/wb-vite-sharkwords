bc479cff703c89611735d20804928e5f20df5e5f
Cat-Mills <141058467+Cat-Mills@users.noreply.github.com>
2023-08-14 16:06:59 -0600

    done!

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import './style.css';
 import getRandomWord from './src/randomWord.js';
+import { setSharkImage } from './src/sharkImage.js';
+import { setupWord } from './src/word.js';
+import { isLetterInWord } from './src/word.js';
+import { revealLetterInWord } from './src/word.js';
+import setupGuesses from './src/guess';
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
 
@@ -9,13 +14,41 @@ document.querySelector('#app').innerHTML = `
 
   <section id="letter-buttons"></section>
 `;
-
+let sharkImgEl = document.querySelector('#shark-img')
 const initSharkwords = () => {
   let numWrong = 0;
   const word = getRandomWord();
+  setSharkImage(sharkImgEl, numWrong);
+  setupWord(document.querySelector('#word-container'), word);
 
+  const handleGuess = (guessEvent,letter) => {
+    const button = guessEvent.target
+    button.setAttribute('disabled',true)
+
+    if(isLetterInWord(letter)){
+      revealLetterInWord(letter)
+    } else {
+      numWrong += 1;
+      setSharkImage(sharkImgEl, numWrong)
+    }
+    if(checkWinner()){
+      alert("You won!")
+      disableButtons()
+    } else if (numWrong > 5){
+      alert("The sharks ate you!")
+      disableButtons()
+    }
+  }
+  function disableButtons (){document.querySelectorAll('button').forEach((btn) => {
+    btn.setAttribute('disabled', true);
+  });}
+  setupGuesses(document.querySelector('#letter-buttons'),handleGuess)
+
+  function checkWinner() {return Array.from(document.querySelectorAll('.letter-box')).every((el)=> el.innerText !== '',)}
+  
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
+  
 };
 
-initSharkwords();
+initSharkwords(setSharkImage);
--- src/guess.js
+++ src/guess.js
@@ -2,9 +2,13 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 function setupGuesses(element, handleGuess) {
   alphabet.split('').forEach((letter) => {
+    //looping over the alphabet
     const btn = document.createElement('button');
+    //making a button for each letter
     btn.innerText = letter;
+    //labeling the button with appropriate letter
     btn.addEventListener('click', (e) => handleGuess(e, letter));
+    //activating the guess function when clicked
     element.append(btn);
   });
 }
